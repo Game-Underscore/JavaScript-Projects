10c670dcf260aef4b457a4e7ffe1b354c9e7655c
Game-Underscore <151592034+Game-Underscore@users.noreply.github.com>
2024-01-23 05:18:21 -0500

    Created Project1_expressions_alert folder

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+var A = "This is the beginning of the string"; //Defined variable A
+var B = " and this is the end of the string."; //Defined variable B
+window.alert(A + B); //Concatenating variables A and B into a window alert
+
+document.write(9 + 10); //Writing a simple expression
